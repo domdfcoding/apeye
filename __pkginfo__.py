1d3a45a3e82cbb96793c422f71f1d81cce05403e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-06 22:08:55 +0100

    Bump version: 0.0.4 → 0.0.5

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 modname = "apeye"
 pypi_name = "apeye"
 import_name = "apeye"
