56e465589886d92e4d504f7c8bc5c7ca93d40e25
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-16 23:15:45 +0100

    Updated config files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description='Handy tools for working with URls and APIs.',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
