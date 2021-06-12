88bb30eaef872b4232b7699f4341155c66942960
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-06-12 14:07:28 +0100

    Integrate the relevant parts of tldextract, but without the code to update and cache the suffix list.

--- apeye/url.py
+++ apeye/url.py
@@ -44,19 +44,532 @@
 #
 
 # stdlib
-import sys
+import collections
+import ipaddress
+import os
+import pathlib
+import re
+from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Optional, Tuple, Type, TypeVar, Union
+from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
+
+# 3rd party
+from domdf_python_tools.doctools import prettify_docstrings
+from domdf_python_tools.typing import PathLike
 
 # this package
-from apeye._url import URL, Domain, URLPath, URLType
-from apeye.requests_url import RequestsURL
-from apeye.slumber_url import SlumberURL
+from apeye import _tld
 
-__all__ = ["URL", "URLPath", "Domain", "RequestsURL", "SlumberURL", "URLType"]
+if TYPE_CHECKING:
+	# stdlib
+	from typing import NoReturn
 
-#: Fiddle __module__ so these *look* like they were defined in this file.
-URL.__module__ = "apeye.url"
-Domain.__module__ = "apeye.url"
-URLPath.__module__ = "apeye.url"
+__all__ = ["URL", "URLPath", "Domain", "URLType"]
 
-if sys.version_info >= (3, 7):  # pragma: no cover (<py37)
-	URLType.__module__ = "apeye.url"
+#: Type variable bound to :class:`~apeye.url.URL`.
+URLType = TypeVar("URLType", bound="URL")
+
+
+@prettify_docstrings
+class URLPath(pathlib.PurePosixPath):
+	"""
+	Represents the path part of a URL.
+
+	Subclass of :class:`pathlib.PurePosixPath` that provides a subset of its methods.
+	"""
+
+	def __str__(self) -> str:
+		"""
+		Return the string representation of the path, suitable for passing to system calls.
+		"""
+
+		try:
+			return self._str  # type: ignore
+		except AttributeError:
+			self._str = self._format_parsed_parts(self._drv, self._root, self._parts) or ''  # type: ignore
+			return self._str
+
+	@classmethod
+	def _format_parsed_parts(cls, drv, root, parts):
+		if drv or root:
+			return drv + root + pathlib._posix_flavour.join(parts[1:])  # type: ignore
+		else:
+			return pathlib._posix_flavour.join(parts)  # type: ignore
+
+	def match(self, *args, **kwargs) -> "NoReturn":  # noqa: D102
+		raise NotImplementedError
+
+	def is_absolute(self, *args, **kwargs) -> "NoReturn":  # noqa: D102
+		raise NotImplementedError
+
+	def joinpath(self, *args, **kwargs) -> "NoReturn":  # noqa: D102
+		raise NotImplementedError
+
+	def relative_to(self, *args, **kwargs) -> "NoReturn":  # noqa: D102
+		raise NotImplementedError
+
+	@property
+	def anchor(self):  # noqa: D102
+		raise NotImplementedError
+
+	@property
+	def drive(self):  # noqa: D102
+		raise NotImplementedError
+
+	def __lt__(self, *args, **kwargs) -> "NoReturn":
+		raise NotImplementedError
+
+	def __le__(self, *args, **kwargs) -> "NoReturn":
+		raise NotImplementedError
+
+	def __gt__(self, *args, **kwargs) -> "NoReturn":
+		raise NotImplementedError
+
+	def __ge__(self, *args, **kwargs) -> "NoReturn":
+		raise NotImplementedError
+
+	def as_uri(self, *args, **kwargs) -> "NoReturn":  # noqa: D102
+		raise NotImplementedError
+
+
+class URL(os.PathLike):
+	r"""
+	:mod:`pathlib`-like class for URLs.
+
+	:param url: The URL to construct the :class:`~apeye.url.URL` object from.
+
+	.. versionchanged:: 0.3.0  The ``url`` parameter can now be a string or a :class:`~.URL`.
+
+	.. autoclasssumm:: URL
+		:autosummary-sections: Methods
+
+	.. autosummary-widths:: 1/5
+
+	.. autoclasssumm:: URL
+		:autosummary-sections: Attributes
+	"""
+
+	#: URL scheme specifier
+	scheme: str
+
+	#: Network location part of the URL
+	netloc: str
+
+	#: The hierarchical path of the URL
+	path: URLPath
+
+	query: Dict[str, List[str]]
+	"""
+	The query parameters of the URL, if present.
+
+	.. versionadded:: 0.7.0
+	"""
+
+	fragment: Optional[str]
+	"""
+	The URL fragment, used to identify a part of the document. :py:obj:`None` if absent from the URL.
+
+	.. versionadded:: 0.7.0
+	"""
+
+	def __init__(self, url: Union[str, "URL"] = ''):
+		if isinstance(url, URL):
+			url = str(url)
+
+		if not re.match("([A-Za-z-.]+:)?//", url):
+			url = "//" + str(url)
+
+		scheme, netloc, parts, params, query, fragment = urlparse(url)
+
+		self.scheme: str = scheme
+		self.netloc: str = netloc
+		self.path = URLPath(parts)
+		self.query = parse_qs(query or '')
+		self.fragment = fragment or None
+
+	@property
+	def port(self) -> Optional[int]:
+		"""
+		The port of number of the URL as an integer, if present. Default :py:obj:`None`.
+
+		.. versionadded:: 0.7.0
+		"""
+
+		if ':' not in self.netloc:
+			return None
+		else:
+			return int(self.netloc.split(':')[-1])
+
+	@classmethod
+	def from_parts(
+			cls: Type[URLType],
+			scheme: str,
+			netloc: str,
+			path: PathLike,
+			query: Optional[Mapping[Any, List]] = None,
+			fragment: Optional[str] = None,
+			) -> URLType:
+		"""
+		Construct a :class:`~apeye.url.URL` from a scheme, netloc and path.
+
+		:param scheme: The scheme of the URL, e.g ``'http'``.
+		:param netloc: The netloc of the URl, e.g. ``'bbc.co.uk:80'``.
+		:param path: The path of the URL, e.g. ``'/news'``.
+		:param query: The query parameters of the URL, if present.
+		:param fragment: The URL fragment, used to identify a part of the document.
+			:py:obj:`None` if absent from the URL.
+
+		Put together, the resulting path would be ``'http://bbc.co.uk:80/news'``
+
+		:rtype:
+
+		.. versionchanged:: 0.7.0  Added the ``query`` and ``fragment`` arguments.
+		"""
+
+		obj = cls('')
+		obj.scheme = scheme
+		obj.netloc = netloc
+		obj.query = dict(query or {})
+		obj.fragment = fragment or None
+
+		path = URLPath(path)
+
+		if path.root == '/':
+			obj.path = path
+		else:
+			obj.path = URLPath('/' + str(path))
+
+		return obj
+
+	def __str__(self) -> str:
+		"""
+		Returns the :class:`~apeye.url.URL` as a string.
+		"""
+
+		query = urlencode(self.query, doseq=True)
+		url = urlunparse([self.scheme, self.netloc, str(self.path), None, query, self.fragment])
+
+		if url.startswith("//"):
+			return url[2:]
+		else:
+			return url
+
+	def __repr__(self) -> str:
+		"""
+		Returns the string representation of the :class:`~apeye.url.URL`.
+		"""
+
+		return f"{self.__class__.__name__}({str(self)!r})"
+
+	def __truediv__(self: URLType, key: Union[PathLike, int]) -> URLType:
+		"""
+		Construct a new :class:`~apeye.url.URL` object for the given child of this :class:`~apeye.url.URL`.
+
+		:rtype:
+
+		.. versionchanged:: 0.7.0
+
+			* Added support for division by integers.
+
+			* Now officially supports the new path having a URL fragment and/or query parameters.
+			  Any URL fragment or query parameters from the parent URL is not inherited by its children.
+		"""
+
+		_key = key
+
+		if isinstance(key, pathlib.PurePath):
+			key = key.as_posix()
+		elif isinstance(key, os.PathLike):
+			key = os.fspath(key)
+		elif isinstance(key, int):
+			key = str(key)
+
+		try:
+			parse_result = urlparse(key)
+		except AttributeError as e:
+			if str(e).endswith("'decode'"):
+				raise TypeError(
+						f"unsupported operand type(s) for /: "
+						f"'{type(self.path).__name__!r}' and {type(_key).__name__!r}",
+						) from None
+			else:
+				raise
+
+		try:
+			new_path = self.from_parts(self.scheme, self.netloc, self.path / parse_result.path)
+		except TypeError:
+			return NotImplemented
+
+		new_path.query = parse_qs(parse_result.query)
+		new_path.fragment = parse_result.fragment or None
+
+		return new_path
+
+	def __fspath__(self) -> str:
+		"""
+		Returns the file system path representation of the :class:`~.URL`.
+		"""
+
+		return f"{self.netloc}{self.path}"
+
+	def __eq__(self, other) -> bool:
+		"""
+		Return ``self == other``.
+
+		.. latex:vspace:: -10px
+
+		.. attention::
+
+			URL fragments and query parameters are not compared.
+
+			.. seealso:: :meth:`.URL.strict_compare`, which *does* consider those attributes.
+
+		.. latex:vspace:: -20px
+		"""
+
+		if isinstance(other, URL):
+			return self.netloc == other.netloc and self.scheme == other.scheme and self.path == other.path
+		else:
+			return NotImplemented
+
+	def strict_compare(self, other) -> bool:
+		"""
+		Return ``self ≡ other``, comparing the scheme, netloc, path, fragment and query parameters.
+
+		.. versionadded:: 0.7.0
+		"""
+
+		if isinstance(other, URL):
+			return (
+					self.netloc == other.netloc and self.scheme == other.scheme and self.path == other.path
+					and self.query == other.query and self.fragment == other.fragment
+					)
+		else:
+			return NotImplemented
+
+	def __hash__(self) -> int:
+		"""
+		Returns the has of the :class:`~apeye.url.URL` .
+		"""
+
+		return hash((self.scheme, self.netloc, self.path))
+
+	@property
+	def name(self) -> str:
+		"""
+		The final path component, if any.
+		"""
+
+		return self.path.name
+
+	@property
+	def suffix(self) -> str:
+		"""
+		The final component's last suffix, if any.
+
+		This includes the leading period. For example: ``'.txt'``.
+		"""
+		return self.path.suffix
+
+	@property
+	def suffixes(self) -> List[str]:
+		"""
+		A list of the final component's suffixes, if any.
+
+		These include the leading periods. For example: ``['.tar', '.gz']``.
+		"""
+		return self.path.suffixes
+
+	@property
+	def stem(self):
+		"""
+		The final path component, minus its last suffix.
+		"""
+
+		return self.path.stem
+
+	def with_name(self: URLType, name: str, inherit: bool = True) -> URLType:
+		"""
+		Return a new :class:`~apeye.url.URL` with the file name changed.
+
+		:param name:
+		:param inherit: Whether the new :class:`~apeye.url.URL` should inherit the query string
+			and fragment from this :class:`~apeye.url.URL`.
+
+		:rtype:
+
+		.. versionchanged:: 0.7.0  Added the ``inherit`` parameter.
+		"""
+
+		if inherit:
+			kwargs = {"query": self.query, "fragment": self.fragment}
+		else:
+			kwargs = {}
+
+		return self.from_parts(
+				self.scheme,
+				self.netloc,
+				self.path.with_name(name),
+				**kwargs,  # type: ignore
+				)
+
+	def with_suffix(self: URLType, suffix: str, inherit: bool = True) -> URLType:
+		"""
+		Returns a new :class:`~apeye.url.URL` with the file suffix changed.
+
+		If the :class:`~apeye.url.URL` has no suffix, add the given suffix.
+
+		If the given suffix is an empty string, remove the suffix from the :class:`~apeye.url.URL`.
+
+		:param suffix:
+		:param inherit: Whether the new :class:`~apeye.url.URL` should inherit the query string
+			and fragment from this :class:`~apeye.url.URL`.
+
+		:rtype:
+
+		.. versionchanged:: 0.7.0  Added the ``inherit`` parameter.
+		"""
+
+		if inherit:
+			kwargs = {"query": self.query, "fragment": self.fragment}
+		else:
+			kwargs = {}
+
+		return self.from_parts(
+				self.scheme,
+				self.netloc,
+				self.path.with_suffix(suffix),
+				**kwargs,  # type: ignore
+				)
+
+	@property
+	def parts(self) -> Tuple[str, ...]:
+		"""
+		An object providing sequence-like access to the components in the URL.
+
+		To retrieve only the parts of the path, use :meth:`URL.path.parts <URLPath.parts>`.
+		"""
+
+		return (
+				self.scheme,
+				self.domain.subdomain,
+				self.domain.domain,
+				self.domain.suffix,
+				*self.path.parts[1:],
+				)
+
+	@property
+	def parent(self: URLType) -> URLType:
+		"""
+		The logical parent of the :class:`~apeye.url.URL`.
+		"""
+
+		return self.from_parts(self.scheme, self.netloc, self.path.parent)
+
+	@property
+	def parents(self: URLType) -> Tuple[URLType, ...]:
+		"""
+		An immutable sequence providing access to the logical ancestors of the :class:`~apeye.url.URL`.
+		"""
+
+		return tuple(self.from_parts(self.scheme, self.netloc, path) for path in self.path.parents)
+
+	@property
+	def fqdn(self) -> str:
+		"""
+		Returns the Fully Qualified Domain Name of the :class:`~apeye.url.URL` .
+		"""
+
+		return self.domain.fqdn
+
+	@property
+	def domain(self) -> "Domain":
+		"""
+		Returns a :class:`apeye.url.Domain` object representing the domain part of the URL.
+		"""
+
+		return Domain._make(_tld.extract_tld(self.netloc))
+
+	@property
+	def base_url(self: URLType) -> URLType:
+		"""
+		Returns a :class:`apeye.url.URL` object representing the URL without query strings or URL fragments.
+
+		.. versionadded:: 0.7.0
+		"""
+
+		return self.from_parts(
+				self.scheme,
+				self.netloc,
+				self.path,
+				)
+
+
+class Domain(collections.namedtuple("Domain", "subdomain, domain, suffix")):
+	"""
+	:class:`typing.NamedTuple` of a URL's subdomain, domain, and suffix.
+	"""
+
+	subdomain: str
+	domain: str
+	suffix: str
+
+	__slots__ = ()
+
+	@property
+	def registered_domain(self):
+		"""
+		Joins the domain and suffix fields with a dot, if they're both set.
+
+		.. code-block:: python
+
+			>>> URL('https://forums.bbc.co.uk').domain.registered_domain
+			'bbc.co.uk'
+			>>> URL('https://localhost:8080').domain.registered_domain
+			''
+		"""
+		if self.domain and self.suffix:
+			return self.domain + '.' + self.suffix
+		return ''
+
+	@property
+	def fqdn(self):
+		"""
+		Returns a Fully Qualified Domain Name, if there is a proper domain/suffix.
+
+		.. code-block:: python
+
+			>>> URL('https://forums.bbc.co.uk/path/to/file').domain.fqdn
+			'forums.bbc.co.uk'
+			>>> URL('https://localhost:8080').domain.fqdn
+			''
+		"""
+		if self.domain and self.suffix:
+			# self is the namedtuple (subdomain domain suffix)
+			return '.'.join(i for i in self if i)
+		return ''
+
+	@property
+	def ipv4(self) -> Optional[ipaddress.IPv4Address]:
+		"""
+		Returns the ipv4 if that is what the presented domain/url is.
+
+		.. code-block:: python
+
+			>>> URL('https://127.0.0.1/path/to/file').domain.ipv4
+			IPv4Address('127.0.0.1')
+			>>> URL('https://127.0.0.1.1/path/to/file').domain.ipv4
+			None
+			>>> URL('https://256.1.1.1').domain.ipv4
+			None
+		"""
+
+		if not (self.suffix or self.subdomain) and _tld.IP_RE.match(self.domain):
+			return ipaddress.ip_address(self.domain)
+		return None
+
+	def __repr__(self) -> str:
+		"""
+		Return a string representation of the :class:`~.Domain`.
+		"""
+
+		return super().__repr__()
---
+++ tests/test_tld.py
@@ -0,0 +1,232 @@
+#  Adapted from https://github.com/john-kurkowski/tldextract
+#  Licensed under the BSD 3-Clause License
+#
+#  Copyright (c) 2020, John Kurkowski
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# stdlib
+from ipaddress import IPv4Address
+
+# this package
+from apeye import Domain
+from apeye._tld import extract_tld
+
+
+def assert_extract(url, expected_domain_data, expected_ip_data=None):
+	(expected_fqdn, expected_subdomain, expected_domain, expected_tld) = expected_domain_data
+	ext = Domain._make(extract_tld(url))
+	assert expected_fqdn == ext.fqdn
+	assert expected_subdomain == ext.subdomain
+	assert expected_domain == ext.domain
+	assert expected_tld == ext.suffix
+	assert expected_ip_data == ext.ipv4
+
+
+def test_american():
+	assert_extract("https://www.google.com", ("www.google.com", "www", "google", "com"))
+
+
+def test_british():
+	assert_extract("https://www.theregister.co.uk", ("www.theregister.co.uk", "www", "theregister", "co.uk"))
+
+
+def test_no_subdomain():
+	assert_extract("https://gmail.com", ("gmail.com", '', "gmail", "com"))
+
+
+def test_nested_subdomain():
+	assert_extract(
+			"https://media.forums.theregister.co.uk",
+			("media.forums.theregister.co.uk", "media.forums", "theregister", "co.uk")
+			)
+
+
+def test_odd_but_possible():
+	assert_extract("https://www.www.com", ("www.www.com", "www", "www", "com"))
+	assert_extract("https://www.com", ("www.com", '', "www", "com"))
+
+
+def test_suffix():
+	assert_extract("com", ('', '', '', "com"))
+	assert_extract("co.uk", ('', '', '', "co.uk"))
+
+
+def test_local_host():
+	assert_extract("https://internalunlikelyhostname/", ('', '', "internalunlikelyhostname", ''))
+	assert_extract("https://internalunlikelyhostname.bizarre", ('', "internalunlikelyhostname", "bizarre", ''))
+
+
+def test_qualified_local_host():
+	assert_extract(
+			"https://internalunlikelyhostname.info/",
+			("internalunlikelyhostname.info", '', "internalunlikelyhostname", "info")
+			)
+	assert_extract(
+			"https://internalunlikelyhostname.information/", ('', "internalunlikelyhostname", "information", '')
+			)
+
+
+def test_ip():
+	assert_extract(
+			"https://216.22.0.192/",
+			('', '', "216.22.0.192", ''),
+			expected_ip_data=IPv4Address("216.22.0.192"),
+			)
+	assert_extract("https://216.22.project.coop/", ("216.22.project.coop", "216.22", "project", "coop"))
+
+
+def test_looks_like_ip():
+	assert_extract("1é", ('', '', "1é", ''))
+
+
+def test_punycode():
+	assert_extract("https://xn--h1alffa9f.xn--p1ai", ("xn--h1alffa9f.xn--p1ai", '', "xn--h1alffa9f", "xn--p1ai"))
+	assert_extract("https://xN--h1alffa9f.xn--p1ai", ("xN--h1alffa9f.xn--p1ai", '', "xN--h1alffa9f", "xn--p1ai"))
+	assert_extract("https://XN--h1alffa9f.xn--p1ai", ("XN--h1alffa9f.xn--p1ai", '', "XN--h1alffa9f", "xn--p1ai"))
+	# Entries that might generate UnicodeError exception
+	# This subdomain generates UnicodeError 'IDNA does not round-trip'
+	assert_extract(
+			"xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w.google.com",
+			(
+					"xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w.google.com",
+					"xn--tub-1m9d15sfkkhsifsbqygyujjrw602gk4li5qqk98aca0w",
+					"google",
+					"com"
+					)
+			)
+	# This subdomain generates UnicodeError 'incomplete punicode string'
+	assert_extract(
+			"xn--tub-1m9d15sfkkhsifsbqygyujjrw60.google.com",
+			(
+					"xn--tub-1m9d15sfkkhsifsbqygyujjrw60.google.com",
+					"xn--tub-1m9d15sfkkhsifsbqygyujjrw60",
+					"google",
+					"com"
+					)
+			)
+
+
+def test_invalid_puny_with_puny():
+	assert_extract(
+			"https://xn--zckzap6140b352by.blog.so-net.xn--wcvs22d.hk",
+			(
+					"xn--zckzap6140b352by.blog.so-net.xn--wcvs22d.hk",
+					"xn--zckzap6140b352by.blog",
+					"so-net",
+					"xn--wcvs22d.hk"
+					)
+			)
+	assert_extract("https://xn--&.so-net.com", ("xn--&.so-net.com", "xn--&", "so-net", "com"))
+
+
+def test_puny_with_non_puny():
+	assert_extract(
+			"https://xn--zckzap6140b352by.blog.so-net.教育.hk",
+			("xn--zckzap6140b352by.blog.so-net.教育.hk", "xn--zckzap6140b352by.blog", "so-net", "教育.hk")
+			)
+
+
+def test_idna_2008():
+	"""Python supports IDNA 2003.
+	The IDNA library adds 2008 support for characters like ß.
+	"""
+	assert_extract("xn--gieen46ers-73a.de", ("xn--gieen46ers-73a.de", '', "xn--gieen46ers-73a", "de"))
+
+
+def test_empty():
+	assert_extract("https://", ('', '', '', ''))
+
+
+def test_scheme():
+	assert_extract("https://mail.google.com/mail", ("mail.google.com", "mail", "google", "com"))
+	assert_extract("ssh://mail.google.com/mail", ("mail.google.com", "mail", "google", "com"))
+	assert_extract("//mail.google.com/mail", ("mail.google.com", "mail", "google", "com"))
+	assert_extract("mail.google.com/mail", ("mail.google.com", "mail", "google", "com"))
+
+
+def test_port():
+	assert_extract("git+ssh://www.github.com:8443/", ("www.github.com", "www", "github", "com"))
+
+
+def test_username():
+	assert_extract("ftp://johndoe:5cr1p7k1dd13@1337.warez.com:2501", ("1337.warez.com", "1337", "warez", "com"))
+
+
+def test_query_fragment():
+	assert_extract("https://google.com?q=cats", ("google.com", '', "google", "com"))
+	assert_extract("https://google.com#Welcome", ("google.com", '', "google", "com"))
+	assert_extract("https://google.com/#Welcome", ("google.com", '', "google", "com"))
+	assert_extract("https://google.com/s#Welcome", ("google.com", '', "google", "com"))
+	assert_extract("https://google.com/s?q=cats#Welcome", ("google.com", '', "google", "com"))
+
+
+def test_regex_order():
+	assert_extract("https://www.parliament.uk", ("www.parliament.uk", "www", "parliament", "uk"))
+	assert_extract("https://www.parliament.co.uk", ("www.parliament.co.uk", "www", "parliament", "co.uk"))
+
+
+def test_unhandled_by_iana():
+	assert_extract("https://www.cgs.act.edu.au/", ("www.cgs.act.edu.au", "www", "cgs", "act.edu.au"))
+	assert_extract("https://www.google.com.au/", ("www.google.com.au", "www", "google", "com.au"))
+
+
+def test_tld_is_a_website_too():
+	assert_extract("https://www.metp.net.cn", ("www.metp.net.cn", "www", "metp", "net.cn"))
+	# This is unhandled by the PSL. Or is it?
+	# assert_extract(https://www.net.cn', ('www.net.cn', 'www', 'net', 'cn'))
+
+
+def test_dns_root_label():
+	assert_extract("https://www.example.com./", ("www.example.com", "www", "example", "com"))
+
+
+def test_private_domains():
+	assert_extract("https://waiterrant.blogspot.com", ("waiterrant.blogspot.com", "waiterrant", "blogspot", "com"))
+
+
+def test_ipv4():
+	assert_extract(
+			"https://127.0.0.1/foo/bar", ('', '', "127.0.0.1", ''), expected_ip_data=IPv4Address("127.0.0.1")
+			)
+
+
+def test_ipv4_bad():
+	assert_extract("https://256.256.256.256/foo/bar", ('', "256.256.256", "256", ''))
+
+
+def test_ipv4_lookalike():
+	assert_extract("https://127.0.0.1.9/foo/bar", ('', "127.0.0.1", '9', ''))
+
+
+def test_result_as_dict():
+	result = Domain._make(
+			extract_tld("https://admin:password1@www.google.com:666/secret/admin/interface?param1=42")
+			)
+	expected_dict = {"subdomain": "www", "domain": "google", "suffix": "com"}
+	assert result._asdict() == expected_dict
